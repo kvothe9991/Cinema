cd9042312c9c5ba3fdf4ea4dc93e67a2f4bdc760
yeyon <eduardo.garcia.maleta@gmail.com>
2021-06-10 12:29:17 -0400

    Añadido Criterio.cs en Data

---
+++ Cine/Data/Criterio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cine.Data
+{
+    public enum Criterio
+    {
+        MasVistas, MasGustadas, InteresEconomico, Aleatorio
+    }
+
+    public static class CriterioActual
+    {
+        public static Criterio Criterio = Criterio.MasNuevas;
+    }
+}
