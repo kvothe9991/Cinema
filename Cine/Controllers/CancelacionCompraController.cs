a162b67afcd889239826bcc4b51b9dd249b8c4ec
yeyon <eduardo.garcia.maleta@gmail.com>
2021-06-10 12:28:46 -0400

    Cambio de nombre de CancelacionCompraModel por CancelacionCompra

---
+++ Cine/Controllers/CancelacionCompraController.cs
@@ -0,0 +1,32 @@
+﻿using Cine.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cine.Models;
+
+
+namespace Cine.Controllers
+{
+    public class CancelacionCompraController : Controller
+    {
+        private CineContext db = new CineContext();
+
+        // GET: CancelacionCompra
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            CancelacionCompra ccm = new CancelacionCompra { };
+
+            // Ricardo le mete query a db buscando id
+            // cancela la compra
+            // setea ccm
+
+            return View(ccm);
+        }
+    }
+}
