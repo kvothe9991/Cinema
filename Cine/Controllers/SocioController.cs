3788efa2d5d9491fc656131056d407b89844f860
yeyon <eduardo.garcia.maleta@gmail.com>
2021-06-10 12:30:00 -0400

    Añadido SocioController CRUD

---
+++ Cine/Controllers/SocioController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cine.DAL;
+using Cine.Models;
+
+namespace Cine.Controllers
+{
+    public class SocioController : Controller
+    {
+        private CineContext db = new CineContext();
+
+        // GET: Socio
+        public ActionResult Index()
+        {
+            return View(db.Socios.ToList());
+        }
+
+        // GET: Socio/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        // GET: Socio/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Socio/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SocioID,Puntos,DatosPersonales,UsuarioDescuento")] Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Socios.Add(socio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(socio);
+        }
+
+        // GET: Socio/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        // POST: Socio/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SocioID,Puntos,DatosPersonales,UsuarioDescuento")] Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(socio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(socio);
+        }
+
+        // GET: Socio/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        // POST: Socio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Socio socio = db.Socios.Find(id);
+            db.Socios.Remove(socio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
