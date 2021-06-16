eb2b1fb0a4f19af133436c25ecf93dcd9de9e522
kvothe9991 <kvothe9991@gmail.com>
2021-06-15 23:04:54 -0400

    Revert "Revert "Movido a Bootstrap v5 y añadidas dependencias de Bootstrap. Modificado _Layout.cshtml y varios controladores.""
    This reverts commit 71e8c5dddb122ea6c9d8ec59c7d26eede2c87c70.

--- Cine/Models/CancelacionCompra.cs
+++ Cine/Models/CancelacionCompra.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Cine.Models
 {
-    public class CancelacionCompraModel
+    public class CancelacionCompra
     {
         public int Id { get; set; }
         public bool Cancelable { get; set; }
