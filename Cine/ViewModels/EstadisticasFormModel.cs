d2ca1011381c27182a6e1bb3081acd6258794c5e
kvothe9991 <kvothe9991@gmail.com>
2021-06-16 07:05:02 -0400

    Nuevo Navbar-item, modificaciones a los criterios y Estadisticas

--- Cine/ViewModels/EstadisticasFormModel.cs
+++ Cine/ViewModels/EstadisticasFormModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Cine.Data;
 
 namespace Cine.ViewModels
 {
     public class EstadisticasFormModel
     {
-
+        public CriterioEst criterio;
+        public DateTime desde, hasta;
     }
 }
