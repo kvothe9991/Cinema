d8db30b630ff605a87e9f91bbe5ace6a864d66d4
yeyon <eduardo.garcia.maleta@gmail.com>
2021-06-16 13:00:15 -0400

    Añadido CRUDS de Horario y Sala Borrado bin y obj dentro de Cine

---
+++ Cine/Controllers/SalaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cine.DAL;
+using Cine.Models;
+
+namespace Cine.Controllers
+{
+    public class SalaController : Controller
+    {
+        private CineContext db = new CineContext();
+
+        // GET: Sala
+        public ActionResult Index()
+        {
+            return View(db.Salas.ToList());
+        }
+
+        // GET: Sala/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sala sala = db.Salas.Find(id);
+            if (sala == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sala);
+        }
+
+        // GET: Sala/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sala/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SalaID")] Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Salas.Add(sala);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sala);
+        }
+
+        // GET: Sala/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sala sala = db.Salas.Find(id);
+            if (sala == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sala);
+        }
+
+        // POST: Sala/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SalaID")] Sala sala)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sala).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sala);
+        }
+
+        // GET: Sala/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sala sala = db.Salas.Find(id);
+            if (sala == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sala);
+        }
+
+        // POST: Sala/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sala sala = db.Salas.Find(id);
+            db.Salas.Remove(sala);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
