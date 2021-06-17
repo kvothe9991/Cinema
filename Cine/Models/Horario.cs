a8a8c0e8ee5ea79054c662dbdd7ddf12120f4f7d
RP <61019320+cassius66@users.noreply.github.com>
2021-06-17 03:10:33 -0800

    Horario

---
+++ Cine/Models/Horario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cine.Models
+{
+    public class Horario
+    {
+        public int HorarioID { get; set; }
+        public int HH { get; set; }
+        public int MM { get; set; }
+
+        public virtual ICollection<Filme> Filmes { get; set; }
+    }
+}
